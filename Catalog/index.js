ec9ff155c8d28cdc94227ebdcc65319da558e034
ola-saadeh <olas3ada12@gmail.com>
2023-11-27 08:09:17 +0200

    add searching for book with subject

--- Catalog/index.js
+++ Catalog/index.js
@@ -22,70 +22,15 @@ app.get("/", (req, res) => {
 app.get("/books/subject", async (req, res) => {
     try {
         let subject = req.headers["subject"];
-        customLogger.log("Searching for books with subject: " + subject);
+        console.log("Searching for books with subject: " + subject);
+        customLogger.myLog("Searching for books with subject: " + subject);
         let result = await knex("books").select().where("topic", subject);
-        if (result.length === 0) {
+        if (result.length == 0) {
             res.json({ "result": "No books found in this subject" });
         }
-        customLogger.log("The result is: " + JSON.stringify(result));
+        customLogger.myLog("The result is: " + JSON.stringify(result));
         res.json(result);
     } catch (error) {
         res.status(500).send("Error processing your request on the server");
     }
-});
-
-app.get("/books/item", async (req, res) => {
-    try {
-        let itemNumber = req.headers["itemnumber"];
-        customLogger.log("Searching for the book with item number: " + itemNumber);
-        let result = await knex("books").select().where("number", itemNumber);
-        if (result.length === 0) {
-            res.json({ "result": "No book found with this item number" });
-        }
-        customLogger.log("The result is: " + JSON.stringify(result));
-        res.json(result);
-    } catch (error) {
-        res.status(500).send("Error processing your request on the server");
-    }
-});
-
-app.put("/books/update/stock", async (req, res) => {
-    try {
-        let operation = req.headers["operation"];
-        let amount = req.headers["amount"];
-        let itemNumber = req.headers["itemnumber"];
-        customLogger.log(`Updating item ${itemNumber} stock by ${operation} amount to ${amount}`);
-        let result = await knex("books").select().where("number", itemNumber);
-        if (result.length === 0) {
-            res.json({ "result": "No book found with this item number to update" });
-        }
-        let newVal = (operation === "increase") ? parseInt(result[0]["stock"]) + parseInt(amount) : parseInt(result[0]["stock"]) - parseInt(amount);
-        customLogger.log("The new value is: " + newVal);
-        await knex('books').update('stock', newVal).where("number", itemNumber);
-        customLogger.log("Updated successfully");
-        res.sendStatus(200);
-    } catch (error) {
-        res.status(500).send("Error processing your request on the server");
-    }
-});
-
-app.put("/books/update/cost", async (req, res) => {
-    try {
-        let newCost = parseInt(req.headers["newcost"]);
-        let itemNumber = req.headers["itemnumber"];
-        let result = await knex("books").select().where("number", itemNumber);
-        if (result.length === 0) {
-            res.json({ "result": "No book found with this item number to update" });
-        }
-        customLogger.log(`Updating the book number ${itemNumber} cost to ${newCost}`);
-        await knex('books').update('cost', newCost).where("number", itemNumber);
-        customLogger.log("Updated successfully");
-        res.sendStatus(200);
-    } catch (error) {
-        res.status(500).send("Error processing your request on the server");
-    }
-});
-
-app.listen(process.env.PORT, () => {
-    customLogger.myLog(`Server is running on port ${process.env.PORT}`);
 });
