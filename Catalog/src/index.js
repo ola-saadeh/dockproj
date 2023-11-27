fea07040ee6e93218f4c94ac443f73557b15b5a5
ola-saadeh <olas3ada12@gmail.com>
2023-11-27 05:38:22 +0200

    RESTful endpoints for managing the book catalog.

--- Catalog/src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-//init app
-
-const PORT = process.env.PORT || 4000;
-const app = express();
-app.get('/', (req,res) => res.send('<h1>hi  olaaam dev taty jjjb</h1>'))
- 
-app.listen(PORT, () =>console.log(`app is up and running on port :${PORT}`));
