606d95e360c77898a57cad3722212783eb61ce1f
ola-saadeh <olas3ada12@gmail.com>
2023-12-31 03:19:34 +0200

    add roundrobin to search

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -3,6 +3,9 @@ const path = require("path")
 const cors = require('cors');
 const app = express() ;
 const axios = require("axios");
+
+// Replicated servers
+
 const cat_IP = ["http://" + process.env.First +":4000","http://" + process.env.Second +":4000"]
 const oreder_IP = ["http://" + process.env.First +":5000","http://" + process.env.Second +":5000"]
 
@@ -11,6 +14,9 @@ let catlogIP = "http://localhost:4000";
 let orderIP = "http://localhost:5000";
 console.log(process.env.HOST)
  
+
+// Load balancing algorithm (Round Robin)
+
 let orderServerIndex = 0;
 let catalogServerIndex = 0;
 
@@ -23,6 +29,10 @@ const customLogger = require("./custom-logger");
 app.get("/",(req,res)=>{
     res.sendStatus(200)
 })
+
+
+    // Check cache first
+
 const collection = new Map();
 let cashMid = (req, res, next)=>{
     
@@ -50,11 +60,12 @@ let cashMid = (req, res, next)=>{
 app.get("/search",cashMid,(req,res)=>{
     let subject = req.headers["topic"]
     customLogger.myLog("Querying books related to the topic "+subject)
-    customLogger.myLog("Exploring on server : " + catlogIP )
-    axios.get(catlogIP+'/books/subject',{headers:{subject}})
+    customLogger.myLog("Exploring on server : " + cat_IP[catalogServerIndex] )
+    axios.get(cat_IP[catalogServerIndex]+'/books/subject',{headers:{subject}})
         .then((ress)=> {
             collection.set(req.headers["topic"] + req.url,ress.data)
-         
+            catalogServerIndex = (catalogServerIndex + 1) % cat_IP.length;
+
             customLogger.myLog("Results for the Search : " + JSON.stringify(ress.data) )
            res.send(ress.data)
            
@@ -69,15 +80,18 @@ app.get("/search",cashMid,(req,res)=>{
 app.get("/info",cashMid,(req,res)=>{
     let booknumber = req.headers["booknumber"]
     customLogger.myLog("Querying books related to the book number : " + booknumber);
-    customLogger.myLog("Exploring on server : " + catlogIP);
-    axios.get(catlogIP +'/books/item', {headers:{booknumber}})
+    customLogger.myLog("Exploring on server : " + cat_IP[catalogServerIndex]);
+    axios.get(cat_IP[catalogServerIndex] +'/books/item', {headers:{booknumber}})
         .then((ress)=> {
             
             collection.set(req.headers["booknumber"] + req.url,ress.data[0])
+
+            catalogServerIndex = (catalogServerIndex + 1) % cat_IP.length;
+
             customLogger.myLog("the book is: "+ JSON.stringify(ress.data[0]))
          
-          
-                res.send(ress.data)
+            const responseData = !ress.data[0] ? ress.data : ress.data[0];
+            res.send(responseData);
             
         })
         .catch( (error) =>{
